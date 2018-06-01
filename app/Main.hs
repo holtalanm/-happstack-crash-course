549c4f23c02d244b4542b6f152846ff9b113a4d6
Alan Holt <holtalanm@gmail.com>
2018-05-31 22:28:59 -0500

    progress

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,26 @@
 module Main where
 
-import Control.Monad
-import Happstack.Server (nullConf, simpleHTTP, toResponse, ok, dir)
+import Control.Monad (msum)
+import Data.Char (toLower)
+import Happstack.Server (Method(GET, POST), FromReqURI(..), nullConf, simpleHTTP, ok, dir, path, method)
+
+data Subject = World | Haskell
+
+sayHello :: Subject -> String
+sayHello World = "Hello, World!"
+sayHello Haskell = "Greetings, Haskell!"
+
+instance FromReqURI Subject where
+    fromReqURI sub =
+        case map toLower sub of
+            "haskell" -> Just Haskell
+            "world"   -> Just World
+            _         -> Nothing
 
 main :: IO ()
-main = simpleHTTP nullConf $ msum [ mzero
-                                  , ok "Hello, World!"
-                                  , ok "Unreachable"
+main = simpleHTTP nullConf $ msum [ dir "hello" $ path $ \subject -> ok $ (sayHello subject)
+                                  , dir "methods" $ do method GET
+                                                        ok $ "GET Method"
+                                  , dir "methods" $ do method POST
+                                                        ok $ "POST Method"
                                   ]
